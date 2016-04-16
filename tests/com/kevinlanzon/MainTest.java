12e3b96171f0e97795a3779c0fe2d1eb89a861dc
Kevin Lanzon <kevinlanzon@gmail.com>
2016-04-16 12:59:37 +0100

    initial commit sets up file structure

---
+++ tests/com/kevinlanzon/MainTest.java
@@ -0,0 +1,14 @@
+package com.kevinlanzon;
+
+import org.junit.Test;
+
+/**
+ * Created by kevinlanzon on 16/04/2016.
+ */
+public class MainTest {
+    @Test
+    public void main() throws Exception {
+
+    }
+
+}
