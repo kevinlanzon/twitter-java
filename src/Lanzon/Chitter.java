fa0e4f2a66134c1fbc07bcbfbe5dcf91dd3718d2
Kevin Lanzon <kevinlanzon@gmail.com>
2016-04-17 14:03:52 +0100

    console displays welcome message

---
+++ src/Lanzon/Chitter.java
@@ -0,0 +1,8 @@
+package Lanzon;
+
+public class Chitter {
+
+    public static void main(String[] args) {
+        System.out.print("Welcome to Chitter!");
+    }
+}
---
+++ tests/Lanzon/ChitterTest.java
@@ -0,0 +1,37 @@
+package Lanzon;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
+public class ChitterTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+        System.setErr(null);
+    }
+    String[] args = {};
+    Chitter chitter = new Chitter();
+
+    @Test
+    public void Message() {
+        chitter.main(args);
+        assertEquals("Welcome to Chitter!", outContent.toString());
+    }
+
+}
